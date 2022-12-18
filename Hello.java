db131379bdbe0a2123fb6be011862af1cc9714f8
Ariike.ir <shaida.ghavam@gmail.com>
2022-12-18 15:59:40 +0330

    first commit

---
+++ Hello.java
@@ -0,0 +1,79 @@
+/**
+ * Hello , this is the first homework
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Zahra");
+
+        // Variable      
+        
+        String textTest = "String is a class";
+        System.out.println(textTest);
+
+        // .....+.....
+
+        String massage = "you are ";  String massage_edit = "beutiful";
+        System.out.printf(massage+massage_edit);
+
+        /*
+         * java Data Types
+         * byte   from -128 to 127
+         * 1 byte
+         */
+        byte room_num = 56;  byte temperature = -7;
+
+        /* short
+         *  2 bytes 
+         * -32,768 to 32,767
+         */
+        short Tshirt_price = 20999; short discount = -31978;
+
+        /* int 
+         * 4 bytes
+         * -2,147,483,648 to 2,147,483,647
+         */
+
+         int bank_loan = 128653423;  int debt= -752456789;
+   
+         /* long 
+          * 8 bytes
+          * -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+          */ 
+
+         long country_budget = 5707652430776739L; long country_debt = -97363400987654779L;
+
+         /* float
+          *  4 bytes
+          *6 to 7 decimal digits
+          */
+        float weight = 45.764f; float weight_loss = -2.975f;
+
+        /* double
+         * 15 decimal digits
+         * 8 bytes
+         */
+        
+         double statistics = 23.323567087f; double statistics_descrease = -63.098765446789065f;
+
+         /*boolean
+          * 1 bit 
+          true or false 
+          */
+
+          boolean  my_bool = false;  boolean my_bool_2 = true;
+
+        /* char
+        letter or ASCII values
+         * 2 bytes
+         */
+        char my_first_name_letter = 'Z'; char my_last_name_letter = 'A';
+
+      
+        
+    }
+}
+    
+
+
+
